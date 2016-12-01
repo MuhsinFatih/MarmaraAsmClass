d176e2a1151becaba08679ff67ebf59228697e4c
MuhsinFatih <muhsinfatih@gmail.com>
2016-12-01 15:49:57 +0300

    random

--- lecture.asm
+++ lecture.asm
@@ -11,27 +11,22 @@ INCLUDE Irvine32.inc
 
 .data
 
-array	sdword	-5,3,9,-2,-6,6,7
-str1	byte	"# of even numbers:",0
 
 .code
 main PROC
 
-	mov ecx, lengthof array
-	mov ebx, 0	;index
-	mov eax, 0	;counter for even numbers
 
+call randomize
+
+mov ecx, 10
 start:
-	test array[ebx], 00000000000000000000000000000001
-	jnz cont
-	inc eax
-cont:
-	add ebx, 4
-	loop start
-
-	mov edx, offset str1
-	call writestring
+	;	RANDOM:
+	mov eax, 100
+	call randomrange
 	call writeint
+	call crlf
+
+	loop start
 
 
 
