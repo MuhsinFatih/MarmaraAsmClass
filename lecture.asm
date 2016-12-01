35629fa718e31bceda4612edea77c36efbfe07ab
MuhsinFatih <muhsinfatih@gmail.com>
2016-12-01 14:24:54 +0300

    commit

--- lecture.asm
+++ lecture.asm
@@ -8,7 +8,7 @@ INCLUDE Irvine32.inc
 
 
 /*
-	while (ax != bx)
+	while (ax != bx) or (cx < 100)
 		ax = ax - 1
 		bx = bx + 1
 		cx = cx * 2
@@ -23,13 +23,17 @@ INCLUDE Irvine32.inc
 
 start:
 	cmp ax, bx
-	je exit
+	jne while
+	jne while
+	cmp cx, 100
+	jnl _exit
+while:
 	dec ax
 	inc bx
 	add cx,cx ; lol. dont know multiplication yet
 	jmp start
 
-exit:
+_exit:
 	
 
 
