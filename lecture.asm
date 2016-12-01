34b10227c2e97f45893081ce52e960f157a0c2a9
MuhsinFatih <muhsinfatih@gmail.com>
2016-12-01 15:17:38 +0300

    more efficient

--- lecture.asm
+++ lecture.asm
@@ -12,42 +12,29 @@ INCLUDE Irvine32.inc
 .data
 
 array	sdword	-5,3,9,-2,-6,6,7
-str1	byte	"Total of - numbers:",0
-str2	byte	"Total of - numbers:",0
+str1	byte	"# of even numbers:",0
 
 .code
 main PROC
 
-	mov eax, 0					; total of neg numbers
-	mov ebx, 0					; total of pos numbers
-
 	mov ecx, lengthof array
-	mov edi, 0					;index for array
+	mov ebx, 0	;index
+	mov eax, 0	;counter for even numbers
 
 start:
-	cmp array[edi],0			;sign flag will give whether x<0 or not
-	jl negative
-	add ebx, array[edi]
-	jmp continue	
-
-negative:
-	add eax, array[edi]
-continue:
-	add edi, 4
+	test array[ebx], 00000000000000000000000000000001
+	jnz cont
+	inc eax
+cont:
+	add ebx, 4
 	loop start
 
 	mov edx, offset str1
 	call writestring
 	call writeint
 
-	call crlf
 
-	mov edx, offset str2
-	call writestring
-	mov eax, ebx
-	call writeint
-		
-	call DumpRegs
+
 
 	exit
 
