4028ff544f3447066751e7dfec9b862774a145fc
MuhsinFatih <muhsinfatih@gmail.com>
2016-12-08 20:19:09 +0300

    signed division

--- lecture.asm
+++ lecture.asm
@@ -15,26 +15,20 @@ INCLUDE Irvine32.inc
 .code
 main PROC
 
-	; unsigned division (div)
+	; signed division (div)
 	; div x	(r/m)
-	; x -> 32bit
-
-	mov edx, ebx
-	shr edx, 16
-	mov ax, bx
 	
-	; OR
 
-	mov ax, bx
-	shr ebx, 16
-	mov dx, bx
-
-	; OR
+	cbw ; (convert byte to word)
+	; al --> ax (sign extend)
 	
-	; ebx = 12345678h
-	push ebx
-	pop ax ; ax = 5678h
-	pop dx ; dx = 1234h
+	cwd ; (convert word to doubleword)
+	; ax --> dx:ax (sign extend)
+
+	cdq ; (convert doubleword to quadword)
+	; eax --> edx:eax (sign extend)
+
+	div ...
 
 	exit
 
